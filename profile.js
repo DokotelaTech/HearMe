4bb74c257815894c49d7550bceb1b462661c9f5d
Zuzani <gwamazuzani67@gmail.com>
2026-04-19 19:09:17 +0200

    user profile

---
+++ profile.js
@@ -0,0 +1,16 @@
+function goTo(page) {
+    window.location.href = page;
+}
+
+function goToEdit() {
+    window.location.href = "editProfile.html";
+}
+
+// function goToActivity() {
+//     window.location.href = "activity.html";
+// }
+/* == SETTNGS TAP === */
+
+function goToSettings() {
+    window.location.href = "settings.html";
+}
