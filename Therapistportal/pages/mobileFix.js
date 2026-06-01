bd819b450b7717813ce8460d32368f8795b27971
Zuzani <gwamazuzani67@gmail.com>
2026-06-01 21:32:16 +0200

    Updated HearMe UI and fixes

---
+++ Therapistportal/pages/mobileFix.js
@@ -0,0 +1,18 @@
+// mobileFixes.js
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const menuBtn = document.getElementById("mobileMenuBtn");
+    const sidebar = document.querySelector(".sidebar");
+
+    if(menuBtn && sidebar){
+
+        menuBtn.addEventListener("click", () => {
+
+            sidebar.classList.toggle("show");
+
+        });
+
+    }
+
+});
